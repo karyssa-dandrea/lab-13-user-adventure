b64e1a7146dfe28b04bf0d89c3dc55197061437f
karyssa-dandrea <karyssadandrea@gmail.com>
2021-10-27 13:16:57 -0700

    added css and my quest pages

--- MAP/map.js
+++ MAP/map.js
@@ -8,16 +8,30 @@ if (user.hp <= 0 || hasCompletedAllQuests(user)){
     window.location.replace('../gameover');
 }
 
-
-
 for (let quest of quests){
+    if (user.completed[quest.id]){
+        displaySpan(quest);
+    } else {
+        displayLink(quest);
+    }
+}
+
+function displayLink(quest){
     const a = document.createElement('a');
     a.href = `../quest/?id=${quest.id}`;
     a.textContent = quest.title;
 
     mapLinks.appendChild(a);
-
 }
+
+// for (let quest of quests){
+//     const a = document.createElement('a');
+//     a.href = `../quest/?id=${quest.id}`;
+//     a.textContent = quest.title;
+
+//     mapLinks.appendChild(a);
+
+// }
 
 function displaySpan(quest){
     const span = document.createElement('span');
